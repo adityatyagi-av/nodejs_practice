d68330f4e18c0867f43f43f5af45f34d257e2586
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-04-17 07:58:16 +0530

    synchronous property of try...catch

--- try_catch.js
+++ try_catch.js
@@ -8,8 +8,29 @@ try {
 
 //try...catch only works for the runtime errors: It won't work if the code is syntatically wrong
 //The JS engine first read the code, then it runs it. the errors that occurs on the reading phase are called "parse-time" errors and are unrecoverable (from inside that code).
-try {
-    {{{
-} catch (error) {
-    console.log("error")
-}
+// try {
+//     {{{
+// } catch (error) {
+//     console.log("error")
+// }
+
+
+//try...catch works synchronously
+//If an exception happens in "scheduled" code, like in setTimeout , then try..catch won't catch it
+// try{
+//     setTimeout(function(){
+//         noVariable;
+//     },1000);
+// } catch (error){
+//     console.log("code won't work");
+// }
+//Reason: The function itself is executed later, when the engine has already left the try...catch construct.To catch an exception inside a scheduled function, try..catch must be inside the function
+
+setTimeout(function(){
+    try{
+        console.log("HEY BUDDY");
+        loca;
+    } catch(error){
+        console.log("error occured");
+    }
+},2000)
